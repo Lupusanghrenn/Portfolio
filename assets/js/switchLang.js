4f0fb6d3088266fa4fc300c3209872cce9eaac75
Raphael GAMAGORA <raphael.courbier@univ-lyon2.fr>
2019-10-11 12:56:32 +0200

    Ameliorationn api git / switch lang not working

--- assets/js/switchLang.js
+++ assets/js/switchLang.js
@@ -4,6 +4,7 @@ Fichier js qui permet de switch entre le fr et l anglais
 
 function switchFr(){
 	console.log("switchFR");
+	var style = document.createElement("style");
 	var styleSheet=document.styleSheets[0];
 	styleSheet.addRule(".eng","display:none;");
 	styleSheet.addRule(".fr","display:inherit");
@@ -12,8 +13,23 @@ function switchFr(){
 function switchEng(){
 	console.log("switchEng");
 	var styleSheet=document.styleSheets[0];
+	console.log(styleSheet);
 	styleSheet.addRule(".fr","display:none;");
 	styleSheet.addRule(".eng","display:inherit");
+
+	/*var css = 'h1 { background: red; }',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+head.appendChild(style);
+
+style.type = 'text/css';
+if (style.styleSheet){
+  // This is required for IE8 and below.
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
+}*/
 }
 
 function listernetLang(){
@@ -46,6 +62,25 @@ function git() {
 	httpRequest.setRequestHeader("Content-Type", "application/json");
 	httpRequest.addEventListener("load", gitListener);
 	httpRequest.send();
+
+	var httpRequest2 = new XMLHttpRequest();
+	var url="https://api.github.com/users/Kitolex/repos?sort=full_name";
+	httpRequest2.open("GET", url, true);
+	httpRequest2.setRequestHeader("Content-Type", "application/json");
+	httpRequest2.addEventListener("load", function(event){
+		var tab=JSON.parse(event.currentTarget.responseText);
+		var tmp=[];
+		tmp.currentTarget=[];
+		var i=0;
+		var project = tab[i];
+		while(project.name!="JulietteSortDeSaChambre"){
+			i++;
+			project=tab[i];
+		}
+		tmp.currentTarget.responseText=JSON.stringify([project]);
+		gitListener(tmp);
+	});
+	httpRequest2.send();
 }
 
 function gitListener(event){
@@ -55,16 +90,38 @@ function gitListener(event){
 
 	var ul = document.getElementById("githubUl");
 
-	console.log(ul);
-
 	for (var i = 0; i < tab.length; i++) {
 		var currentRepos = tab[i];
 		var li = document.createElement("li");
 		//li.style="padding-bottom:5%";
 		li.style="";
-		var span1 = document.createElement("span");
-		span1.setAttribute("class","icon fa-graduation-cap 12u");
-		span1.innerHTML="  "+currentRepos.name;
+		var span1 = document.createElement("a");
+		span1.setAttribute("href","https://www.github.com/"+currentRepos.owner.login+"/"+currentRepos.name);
+		span1.setAttribute("target","_BLANK");
+
+		switch(currentRepos.language){
+			case "JavaScript":
+				span1.setAttribute("class","icon fa-js 12u");
+				break;
+			case "HTML":
+				span1.setAttribute("class","icon fa-js 12u");
+				break;
+			case "C#":
+				span1.setAttribute("class","icon fa-gamepad 12u");
+				break;
+			case null:
+				span1.setAttribute("class","icon fa-gamepad 12u");
+				break;
+			case "Java":
+				span1.setAttribute("class","icon fa-code 12u");
+				break;
+			case "C++":
+				span1.setAttribute("class","icon fa-image 12u");
+				break;
+			default:
+				span1.setAttribute("class","icon fa-graduation-cap 12u");
+		}
+		span1.innerHTML+="  "+currentRepos.name;
 		var br=document.createElement("br");
 		var span2 = document.createElement("span");
 		//span2.setAttribute("class","pull-right");
@@ -74,7 +131,6 @@ function gitListener(event){
 		li.appendChild(br);
 		li.appendChild(span2);
 		ul.appendChild(li);
-		console.log(currentRepos.name);
 	}
 }
 
